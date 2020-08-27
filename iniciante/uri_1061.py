3e5b2b1765b06b7f57a53ac7c2e045e853eb60ea
Victor Carnaval <victor.carnaval@season.com.br>
2020-08-27 10:51:29 -0300

    exercise done

--- iniciante/uri_1061.py
+++ iniciante/uri_1061.py
@@ -1,5 +1,26 @@
 start_date = input()
-start_time = [int(x) for x in input().split(' : ')]
+[start_hour, start_min, start_sec] = [int(x) for x in input().split(' : ')]
 end_date = input()
-end_time = [int(x) for x in input().split(' : ')]
+[end_hour, end_min, end_sec] = [int(x) for x in input().split(' : ')]
 
+seconds = end_sec - start_sec
+minutes = end_min - start_min
+hours = end_hour - start_hour
+days = int(end_date[4:]) - int(start_date[4:])
+
+if seconds < 0:
+    seconds += 60
+    minutes -= 1
+
+if minutes < 0:
+    minutes += 60
+    hours -= 1
+
+if hours < 0:
+    hours += 24
+    days -= 1
+
+print('%d dia(s)' %days)
+print('%d hora(s)' %hours)
+print('%d minuto(s)' %minutes)
+print('%d segundo(s)' %seconds)
